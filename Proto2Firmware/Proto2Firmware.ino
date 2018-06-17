f4ced995c5289e42176533bc389ea0c16299a369
haooken <2482501+haooken@users.noreply.github.com>
2018-06-17 14:59:28 -0400

    Updating with button read loop time test code

--- Proto2Firmware/Proto2Firmware.ino
+++ Proto2Firmware/Proto2Firmware.ino
@@ -9,6 +9,20 @@
 #define BTN_ROW_0 14
 #define BTN_ROW_1 15
 #define BTN_ROW_2 16
+#define BTN_ROW_3 17
+#define BTN_ROW_4 18
+#define BTN_ROW_5 19
+#define BTN_ROW_6 20
+#define BTN_ROW_7 21
+#define BTN_ROW_8 22
+#define BTN_ROW_9 23
+#define BTN_ROW_10 33
+#define BTN_ROW_11 34
+#define BTN_ROW_12 35
+#define BTN_ROW_13 36
+#define BTN_ROW_14 37
+#define BTN_ROW_15 38
+
 
 #define ROW_COUNT 16
 
@@ -63,6 +77,9 @@ uint8_t btns_copy[16][16] = {
 
 volatile byte col = 0;
 
+unsigned long startTime = 0;
+unsigned long endTime = 0;
+
 void check_btns(){
     // set_col(col);
     sr.setAllHigh();
@@ -70,7 +87,7 @@ void check_btns(){
     
     btns[0][col] = !digitalRead(BTN_ROW_0);
     btns[1][col] = !digitalRead(BTN_ROW_1);
-    /*btns[2][col] = !digitalRead(BTN_ROW_2);
+    btns[2][col] = !digitalRead(BTN_ROW_2);
     btns[3][col] = !digitalRead(BTN_ROW_3);
     btns[4][col] = !digitalRead(BTN_ROW_4);
     btns[5][col] = !digitalRead(BTN_ROW_5);
@@ -83,10 +100,15 @@ void check_btns(){
     btns[12][col] = !digitalRead(BTN_ROW_12);
     btns[13][col] = !digitalRead(BTN_ROW_13);
     btns[14][col] = !digitalRead(BTN_ROW_14);
-    btns[15][col] = !digitalRead(BTN_ROW_15);*/
+    btns[15][col] = !digitalRead(BTN_ROW_15);  //(╯°□°)╯︵ ┻━┻
   
     col++;
-    if(col >= 16){ col = 0; }
+    if(col >= 16){
+      col = 0;
+      endTime = micros();
+      Serial.println(endTime - startTime);
+      startTime = micros();
+    }
     
 }
 
@@ -98,6 +120,19 @@ void setup() {
     pinMode(BTN_ROW_0, INPUT_PULLUP);
     pinMode(BTN_ROW_1, INPUT_PULLUP);
     pinMode(BTN_ROW_2, INPUT_PULLUP);
+    pinMode(BTN_ROW_3, INPUT_PULLUP);
+    pinMode(BTN_ROW_4, INPUT_PULLUP);
+    pinMode(BTN_ROW_5, INPUT_PULLUP);
+    pinMode(BTN_ROW_6, INPUT_PULLUP);
+    pinMode(BTN_ROW_7, INPUT_PULLUP);
+    pinMode(BTN_ROW_8, INPUT_PULLUP);
+    pinMode(BTN_ROW_9, INPUT_PULLUP);
+    pinMode(BTN_ROW_10, INPUT_PULLUP);
+    pinMode(BTN_ROW_11, INPUT_PULLUP);
+    pinMode(BTN_ROW_12, INPUT_PULLUP);
+    pinMode(BTN_ROW_13, INPUT_PULLUP);
+    pinMode(BTN_ROW_14, INPUT_PULLUP);
+    pinMode(BTN_ROW_15, INPUT_PULLUP);
 
     if(!btn_timer.begin(check_btns, 100)) {
         Serial.println("Failed timer init!");
@@ -109,8 +144,8 @@ void loop() {
     for(int i = 0; i < ROW_COUNT; i++){
       for(int j = 0; j < 16; j++){
         if(btns[i][j] > 0){
-          Serial.print("ROW: ");Serial.print(i);
-          Serial.print(" COL: ");Serial.println(j);
+          //Serial.print("ROW: ");Serial.print(i);
+          //Serial.print(" COL: ");Serial.println(j);
         }
       }
     }
