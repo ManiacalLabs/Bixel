636fd008627eeffbde98110a8809630c2f5fe9da
Adam Haile <adammhaile@gmail.com>
2018-06-19 20:36:52 -0400

    buttons and leds at same time!

--- MultiplexTest/MultiPlexTest.ino
+++ MultiplexTest/MultiPlexTest.ino
@@ -1,58 +1,19 @@
+#include <FastLED.h>
 #include "ShiftRegister74HC595.h" // Docs -> https://shiftregister.simsso.de/
-// #include "TimerOne.h"
+#include "globals.h"
 
-#define SHIFT_REGS 2
-#define SHIFT_DATA 0
-#define SHIFT_LATCH 1
-#define SHIFT_CLOCK 2
+FASTLED_USING_NAMESPACE
 
-#define BTN_ROW_0 14
-#define BTN_ROW_1 15
-#define BTN_ROW_2 16
-#define BTN_ROW_3 17
-#define BTN_ROW_4 18
-#define BTN_ROW_5 19
-#define BTN_ROW_6 20
-#define BTN_ROW_7 21
-#define BTN_ROW_8 22
-#define BTN_ROW_9 23
-#define BTN_ROW_10 24
-#define BTN_ROW_11 25
-#define BTN_ROW_12 26
-#define BTN_ROW_13 27
-#define BTN_ROW_14 28
-#define BTN_ROW_15 29
-
-#define BZR_PIN 30
-
-
-#define ROWS 16
-#define COLS 16
-
+// Button Multiplexing
 ShiftRegister74HC595 sr(SHIFT_REGS, SHIFT_DATA, SHIFT_CLOCK, SHIFT_LATCH);
 IntervalTimer btn_timer;
 
-uint8_t col_val[] = { 0xFF };
-inline void set_col(byte c) {
-    // col_val[0] = 0xFF & _BV(c+5);
-    // sr.setAll(col_val);
-    sr.setAllLow();
-    sr.set(c, HIGH);
-}
-
-volatile uint16_t btn_reads[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-volatile uint16_t btns[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+// FastLED Stuff
+CRGB leds[NUM_LEDS];
 
 volatile byte col = 0;
 
-unsigned long startTime = 0;
-unsigned long endTime = 0;
-
-#define read_btn(p) !digitalReadFast(p)
-#define btn_val(x, y) (bool)bitRead(btns[x], y)
-
 void check_btns(){
-    // set_col(col);
     sr.setAllHigh();
     sr.set(15 - col, LOW);
 
@@ -86,11 +47,7 @@ void check_btns(){
 
 }
 
-void setup() {
-    Serial.begin(115200);
-
-    while(!Serial){}
-
+inline void setup_btns() {
     pinMode(BTN_ROW_0, INPUT_PULLUP);
     pinMode(BTN_ROW_1, INPUT_PULLUP);
     pinMode(BTN_ROW_2, INPUT_PULLUP);
@@ -113,16 +70,36 @@ void setup() {
     }
 }
 
+inline void setup_leds() {
+  FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    while(!Serial){}
+
+    setup_leds();
+    setup_btns();
+}
+
 
 void loop() {
     static uint8_t x, y;
+    fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
     for(y = 0; y < ROWS; y++){
       for(x = 0; x < COLS; x++){
         if(btn_val(x, y)){
+          leds[XY(x,y)] = CRGB(255,0,0);
           Serial.print(x);Serial.print(",");Serial.println(y);
         }
       }
     }
-    delay(50);
-    noTone(BZR_PIN);
+
+    FastLED.show();
+    FastLED.delay(50);
+    // delay(50);
 }
