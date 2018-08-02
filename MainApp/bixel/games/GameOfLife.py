dfbf7de5570304039b6b54f365db48c688edd449
Adam Haile <adammhaile@gmail.com>
2018-08-02 20:58:39 +0000

    numpy game of life

--- MainApp/bixel/games/GameOfLife.py
+++ MainApp/bixel/games/GameOfLife.py
@@ -3,8 +3,86 @@ import copy
 from collections import deque
 import time
 import threading
+import numpy as np
 from . base import BaseGame
 from .. import colors
+
+
+# https://www.labri.fr/perso/nrougier/teaching/numpy/numpy.html
+
+# Z = [[0,0,0,0,0,0],
+#      [0,0,0,1,0,0],
+#      [0,1,0,1,0,0],
+#      [0,0,1,1,0,0],
+#      [0,0,0,0,0,0],
+#      [0,0,0,0,0,0]]
+
+# def compute_neighbours(Z):
+#     rows,cols = len(Z), len(Z[0])
+#     N  = [[0,]*(cols)  for i in range(rows)]
+#     for x in range(1,cols-1):
+#         for y in range(1,rows-1):
+#             N[y][x] = Z[y-1][x-1]+Z[y][x-1]+Z[y+1][x-1] \
+#                     + Z[y-1][x]            +Z[y+1][x]   \
+#                     + Z[y-1][x+1]+Z[y][x+1]+Z[y+1][x+1]
+#     return N
+
+# def show(Z):
+#     for l in Z[1:-1]: print l[1:-1]
+#     print
+
+# def iterate(Z):
+#     rows,cols = len(Z), len(Z[0])
+#     N = compute_neighbours(Z)
+#     for x in range(1,cols-1):
+#         for y in range(1,rows-1):
+#             if Z[y][x] == 1 and (N[y][x] < 2 or N[y][x] > 3):
+#                 Z[y][x] = 0
+#             elif Z[y][x] == 0 and N[y][x] == 3:
+#                 Z[y][x] = 1
+#     return Z
+
+# show(Z)
+# for i in range(4):
+#     iterate(Z)
+# show(Z)
+
+class NPTable:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.genNewTable()
+
+    def genNewTable(self):
+        # rand 2d array with values between 0 and 1
+        self.table = np.random.randint(0, 2, (self.height, self.width))
+
+    def compute_neighbors(self):
+        Z = self.table
+        shape = len(Z), len(Z[0])
+        N  = [[0,]*(shape[0])  for i in range(shape[1])]
+        for x in range(1,shape[0]-1):
+            for y in range(1,shape[1]-1):
+                N[x][y] = Z[x-1][y-1]+Z[x][y-1]+Z[x+1][y-1] \
+                        + Z[x-1][y]            +Z[x+1][y]   \
+                        + Z[x-1][y+1]+Z[x][y+1]+Z[x+1][y+1]
+        return N
+
+    def turn(self):
+        Z = self.table
+        rows,cols = len(Z), len(Z[0])
+        N = self.compute_neighbors()
+        for x in range(1,cols-1):
+            for y in range(1,rows-1):
+                if Z[y][x] == 1 and (N[y][x] < 2 or N[y][x] > 3):
+                    Z[y][x] = 0
+                elif Z[y][x] == 0 and N[y][x] == 3:
+                    Z[y][x] = 1
+        self.table = Z
+
+
+
+
 
 class Table:
 
@@ -111,16 +189,15 @@ class Table:
 
 class GameOfLife(BaseGame):
 
-    def setup(self, color=colors.Red, bg=colors.Off, toroidal=False, frames_per_step=10):
+    def setup(self, color=colors.Red, bg=colors.Off, frames_per_step=10):
         self._color = color
         self._bg = bg
-        self.toroidal = toroidal
         self._finishCount = 0
         self.frames_per_step = frames_per_step
 
     def reset(self):
-        self._table = Table(self.matrix.height, self.matrix.width, 1, None)
-        self._table.toroidal = self.toroidal
+        # self._table = Table(self.matrix.height, self.matrix.width, 1, None)
+        self._table = NPTable(self.matrix.height, self.matrix.width)
         self._step = 0
 
     def show_table(self):
@@ -137,20 +214,15 @@ class GameOfLife(BaseGame):
             x = 0
 
     def frame(self):
-        for x, y in self.buttons.int_high():
-            if self._table.table[y][x]:
-                self._table.table[y][x] = 0
-            else:
-                self._table.table[y][x] = 1
+        # for x, y in self.buttons.int_high():
+        #     if self._table.table[y][x]:
+        #         self._table.table[y][x] = 0
+        #     else:
+        #         self._table.table[y][x] = 1
         if self._step % self.frames_per_step == 0:
+            start = time.time()
             self._table.turn()
-            self.show_table()
-            # if self._table.checkStable():
-            #     self._finishCount += 1
-            #     if self._finishCount > 10:
-            #         self._table.genNewTable()
-            #         self._finishCount = 0
-            #         self.animComplete = True
-        else:
-            self.show_table()
+            print(time.time() - start)
+
+        self.show_table()
         self._step += 1
