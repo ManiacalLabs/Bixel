5223f83f7c84d1a9a00b48109126550ff09e2e9f
Adam Haile <adammhaile@gmail.com>
2018-08-13 19:15:16 +0000

    working missile command

--- MainApp/bixel/games/MissileCommand.py
+++ MainApp/bixel/games/MissileCommand.py
@@ -7,57 +7,81 @@ from random import randint
 class MissileCommand(BaseGame):
     def setup(self):
         self._tail = 4
-        self._frames_per_missle_move = 10
+        self._frames_per_missle_move = 15
 
     def reset(self):
         self.buildings = [randint(0, 3) for _ in range(16)]
         self.missiles = []
         self.explosions = []
         self._step = 0
+        self.end_game = False
 
-    def _draw_missile(self, x, y, color):
-        for i in range(self._tail):
+    def _draw_missile(self, x, y):
+        self.matrix.set(x, y, colors.Red)
+        for i in range(1, self._tail + 1):
             if y - i >= 0 and y - i < 16:
                 level = 255 - ((255 // self._tail) * i)
-                self.matrix.set(x, y - i, colors.color_scale(color, level))
+                self.matrix.set(x, y - i, colors.color_scale(colors.DarkOrange, level))
 
     def _draw_missiles(self):
         for m in self.missiles:
-            self._draw_missile(m[0], m[1], colors.Red)
+            self._draw_missile(m[0], m[1])
 
-    def _draw_explosions(self, update):
+    def _draw_explosions(self):
         exp = []
         for e in self.explosions:
-            self.matrix.drawCircle(e[0], e[1], e[2], colors.Green)
-            if update:
-                if e[2] <= 5:
-                    exp.append((e[0], e[1], e[2] + 1))
-        if update:
-            self.explosions = exp
+            self.matrix.drawCircle(e[0], e[1], int(e[2]), e[3])
+            if int(e[2]) <= 3:
+                exp.append((e[0], e[1], e[2] + 0.4, e[3]))
+        self.explosions = exp
+
+    def _check_missile_press(self):
+        res = []
+        for m in self.missiles:
+            if self.buttons.get(m[0], m[1]):
+                self._add_explosion(m[0], m[1], colors.Fuchsia)
+            else:
+                res.append(m)
+        self.missiles = res
+
+    def _add_explosion(self, x, y, color):
+        self.explosions.append((x, y, 1, color))
 
     def frame(self):
         self.matrix.clear()
-        for i in range(16):
-            self.matrix.drawLine(i, 15, i, 15 - self.buildings[i], colors.Blue)
-
-        if self._step % self._frames_per_missle_move == 0:
-            missiles = []
-            for m in self.missiles:
-                if m[1] <= 15:
-                    m_next = m[1] + 1
-                    if m_next == 15 - self.buildings[m[0]]:
-                        self.explosions.append((m[0], m_next, 1))
-                    else:
-                        missiles.append((m[0], m_next))
-            self.missiles = missiles
-
-            self._draw_explosions(True)
-            self._draw_missiles()
+        if self.end_game:
+            if randint(0, 1) == 0:
+                self._add_explosion(randint(0, 15), randint(0, 15), colors.Red)
+            self._draw_explosions()
         else:
-            self._draw_explosions(False)
-            self._draw_missiles()
+            if all(b == 0 for b in self.buildings):
+                self.end_game = True
+            else:
+                for i in range(16):
+                    if self.buildings[i]:
+                        self.matrix.drawLine(i, 15, i, 15 - self.buildings[i] + 1, colors.Blue)
 
-        if randint(0, 30) == 0:
-            self.missiles.append((randint(0, 15), -1))
+                if self._step % self._frames_per_missle_move == 0:
+                    missiles = []
+                    for m in self.missiles:
+                        if m[1] <= 15:
+                            m_next = m[1] + 1
+                            if self.buildings[m[0]] and m_next == 16 - self.buildings[m[0]]:
+                                self._add_explosion(m[0], m_next, colors.Green)
+                                self.buildings[m[0]] -= 1
+                            elif self.buildings[m[0]] == 0 and m_next == 15:
+                                self._add_explosion(m[0], 15, colors.Green)
+                            else:
+                                missiles.append((m[0], m_next))
+                    self.missiles = missiles
+                    self._draw_missiles()
+                else:
+                    self._draw_missiles()
 
-        self._step += 1
+                self._check_missile_press()
+                self._draw_explosions()
+
+                if randint(0, 40) == 0:
+                    self.missiles.append((randint(0, 15), -1))
+
+                self._step += 1
